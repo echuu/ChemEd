8de5d3a9a9c1c829f77d2556a2e30422c2faafa3
echuu <chuu3ric@gmail.com>
2018-10-07 01:12:39 -0500

    preprocess full data done

--- paper_2/preprocess.R
+++ paper_2/preprocess.R
@@ -6,11 +6,11 @@
 
 ## clean up the way the data is presented, add some variables of interest
 
-setwd('/home/eric/Dropbox/chem_ed/data')
+setwd('/home/eric/Dropbox/chem_ed')
 
 library(dplyr)
 
-d = read.csv("p2_data.csv") # 1020 x 56
+d = read.csv("data/p2_data.csv") # 1020 x 56
 
 ## GLOBAL VARS
 n = nrow(d) # number of observations
@@ -39,11 +39,130 @@ for (s in school_names) {
 ## so that we do not completely leave out a school
 table(id_inst) # 7 schools identified by 4-5 letter codes -- 1020 total
 
+# replace the existing school column with the newly encoded school IDs
+d = d %>% mutate(school = id_inst)
+
 
 ##################### ---- add pass/fail variable ---- #########################
 
 d = d %>% mutate(pass = (course > MIN_PASS) + 0) # indicator for pass/fail
 
+
+#####################    process variable values    ############################
+
+## Many of the variables contain values that are incorrectly inputted, creating
+## extra factor levels. Here we manually go through and fix the inputs
+
+# class
+d %>% select(class) %>% table
+d$class[d$class == "SO " ] = "SO"
+d$class = droplevels(d$class)     # drop levels with no observations
+
+# ethnicity (Paper's Count // My Count), * indicates count discrepancy
+# Asian:     78  //  78
+# Black:     60  //  60
+# Hispanic: 267  //  267
+# Mixed:     85  //  86  *
+# White:    493  //  492 * 
+# Other:     36  //  37  *
+
+# Other (Arabic, Asian Indian, Native American, Pacific Islander, Pakistani)
+# note: this new def of the group 'Other' differs slightly from part 1 analysis
+
+length(table(d$ethnic))
+table(d$ethnic)
+ethn_names = names(table(d$ethnic))
+mixed = c("White,Nat Am", "White,PI", "White, Asian", "White,AsianInd",
+          "White,Black", "White,Hisp", " White,NA",
+          "White, NA", "White,Asian", "White,Arab", "Mixed", "Hisp,White",
+          "Hisp,Arab", "Hisp,Asian", "Hisp,AsianInd", "Hisp,Black", "Hisp, NA",
+          "Hisp &White", "Black,White", "Black,Hisp", "Asian,White", 
+          "Asian, PI")
+sum(!(mixed %in% ethn_names)) # check for mis-spelled ethnicities
+
+d$ethnic = as.character(d$ethnic)
+d$ethnic[(d$ethnic %in% mixed)] = "Mixed"  # group Mixed
+
+d$ethnic[d$ethnic == "Whte"] = "White"     # White mispelled
+ethn_groups = c("Other", "Asian", "White", "Mixed", "Hisp", "Black")
+d$ethnic[!(d$ethnic %in% ethn_groups)] = "Other"
+
+
+# gender: 0 (female), 1 (male), -1 (missing)
+table(d$gender)
+sum(is.na(d$gender)) # 10 missing values
+d$gender[is.na(d$gender)] = -1
+d$gender = as.factor(d$gender)
+
+
+# parents
+table(d$parent)
+d$parent = as.character(d$parent)
+d$parent[d$parent == "n"] = "N"
+d$parent[!(d$parent %in% c("N", "Y"))] = "Other"
+d$parent = as.factor(d$parent)
+
+# grands
+table(d$grand)
+d$grand = as.character(d$grand)
+d$grand[d$grand == "n"] = "N"
+d$grand[d$grand == "N "] = "N"
+d$grand[!(d$grand %in% c("N", "Y"))] = "Other"
+d$grand = as.factor(d$grand)
+
+
+### done
+
+
+# version
+str(d$version) 
+d$version = as.factor(d$version) # factor: levels 78, 87
+
+# tx_hs
+d$tx_hs[d$tx_hs != "Y"] = "N"
+d$tx_hs = droplevels(d$tx_hs) # too few N's
+
+# tx_birth
+d$tx_birth[d$tx_birth != "Y"] = "N"
+d$tx_birth = droplevels(d$tx_birth) # too few N's
+
+# hrs
+d$hrs = as.character(d$hrs)
+d$hrs[is.na(d$hrs)] = 0
+# d$hrs[d$hrs == "11-19?"] = "11-19"
+# d$hrs[d$hrs == "11-19?"] = "11-19"
+d$hrs[d$hrs == "11-10"] = "1-10"
+# d$hrs[d$hrs == "0?"] = "0"
+# d$hrs[d$hrs == "-"] = "0"
+d$hrs[d$hrs == ""] = "0"
+d$hrs[d$hrs == "20-39"] = "20-29"
+table(d$hrs) # 6 groups depending on hours working
+d$hrs = as.factor(d$hrs)
+
+# emp_on:
+table(d$emp_on)
+d$emp_on = as.character(d$emp_on)
+d$emp_on[d$emp_on == ""] = "N"
+d$emp_on[d$emp_on == "n"] = "N"
+d$emp_on = as.factor(d$emp_on)
+
+# emp_off:
+table(d$emp_off)
+d$emp_off = as.character(d$emp_off)
+d$emp_off[d$emp_off == ""] = "N"
+d$emp_off[d$emp_off == "B"] = "N"
+d$emp_off[d$emp_off == "T"] = "N"
+d$emp_off = as.factor(d$emp_off)
+
+
+# subset rows containing NA values:
+rows_NA = d[rowSums(is.na(d)) > 0,] # rows are containing mostly variables that
+                                    # we didn't use in the part 1 study
+
+
+write.csv(d, "part2/data/full_data.csv", row.names = FALSE)
+
+# test_read = read.csv("part2/data/full_data.csv")
 
 
 ## end of proceprocess.R
