2b21e72ac893866ad19cd15df27333e0d005ba31
echuu <chuu3ric@gmail.com>
2018-10-23 20:16:40 -0500

    modeling alg and concept

--- paper_2/model.R
+++ paper_2/model.R
@@ -264,35 +264,28 @@ x_train_sub = x_train[,!(names(x0) %in% all_questions)]
 cq_m1 = lm(comm ~ . - alg - conc - pass, x_train_sub)
 summary(cq_m1)
 cq_m1_coeffs = summary(cq_m0)$coefficients
-getMSE(cq_m1, x_test, x_test$comm) # MSE: 
+getMSE(cq_m1, x_test, x_test$comm) # MSE: 3.264922
 
 
 ################################ TO - DO #######################################
 
 ## nonlinear model (linear regression with polynomial term for MUST)
 ## quadratic must term
-cq_m2 = lm(comm ~ . - alg - conc - pass, x_train_sub)
-summary(cq_m1)
-cq_m1_coeffs = summary(cq_m0)$coefficients
-getMSE(cq_m1, x_test, x_test$comm) # MSE: 
+cq_m2 = lm(comm ~ . - alg - conc - pass - must + poly(must, 2), x_train_sub)
+summary(cq_m2)
+cq_m2_coeffs = summary(cq_m2)$coefficients
+getMSE(cq_m2, x_test, x_test$comm) # MSE: 3.270627
 
 ## cubic must term
-cq_m3 = lm(comm ~ . - alg - conc - pass, x_train_sub)
-summary(cq_m1)
-cq_m1_coeffs = summary(cq_m0)$coefficients
-getMSE(cq_m1, x_test, x_test$comm) # MSE: 
-
-## quartic must term
-cq_m4 = lm(comm ~ . - alg - conc - pass, x_train_sub)
-summary(cq_m1)
-cq_m1_coeffs = summary(cq_m0)$coefficients
-getMSE(cq_m1, x_test, x_test$comm) # MSE: 
+cq_m3 = lm(comm ~ . - alg - conc - pass - must + poly(must, 3), x_train_sub)
+summary(cq_m3) # only linear must term
+cq_m3_coeffs = summary(cq_m3)$coefficients
+getMSE(cq_m3, x_test, x_test$comm) # MSE: 3.273161
 
 # ------------------------------------------------------------------------------
 
 
-#### ----- (2) difference (if any) between conceptual vs algorithmic ----- #####
-
+################ ----------- MODEING ALGORITHMIC ----------- ###################
 
 ## algorithmic as a function of MUST + other (linear must term)
     # omit comm since alg contributes to comm
@@ -300,22 +293,38 @@ getMSE(cq_m1, x_test, x_test$comm) # MSE:
 alg_m1 = lm(alg ~ . - comm - conc - pass, x_train_sub)
 summary(alg_m1)
 alg_m1_coeffs = summary(alg_m1)$coefficients
-getMSE(alg_m1, x_test, x_test$alg) # MSE: 
+getMSE(alg_m1, x_test, x_test$alg) # MSE: 1.18494
 
 ## algorithmic as a function of MUST + other (quadratic must term)
+    # omit comm since alg contributes to comm
+    # omit pass, since course average used to determine pass (0/1)
+alg_m2 = lm(alg ~ . - comm - conc - pass - must + poly(must, 2), x_train_sub)
+summary(alg_m2) ## both linear and quadratic fit significant 
+alg_m2_coeffs = summary(alg_m2)$coefficients
+getMSE(alg_m2, x_test, x_test$alg) # MSE: 1.190465
 
 
+################ ----------- MODEING CONCEPTUAL ----------- ####################
 
-## conceptual as a function of MUST + other
-    # omit comm since conc contributes to comm
+## conceptual as a function of MUST + other (linear must term)
+    # omit comm since conc contributes to comm 
     # omit pass, since course average used to determine pass (0/1)
 conc_m1 = lm(conc ~ . - comm - alg - pass, x_train_sub)
 summary(conc_m1)
 conc_m1_coeffs = summary(conc_m1)$coefficients
-getMSE(conc_m1, x_test, x_test$conc) # MSE: 
+getMSE(conc_m1, x_test, x_test$conc) # MSE: 1.325434
 
 
-## conceptual as a function of MUST + other (quadratic must fit)
+## conceptual as a function of MUST + other (quadratic must term)
+    # omit comm since conc contributes to comm
+    # omit pass, since course average used to determine pass (0/1)
+conc_m2 = lm(conc ~ . - comm - alg - pass - must + poly(must, 2), x_train_sub)
+summary(conc_m2)
+conc_m2_coeffs = summary(conc_m2)$coefficients
+getMSE(conc_m2, x_test, x_test$conc) # MSE: 1.324543
+
+
+#### ----- (2) difference (if any) between conceptual vs algorithmic ----- #####
 
 
 ## think of a way to do hypothesis test between using alg and conc
