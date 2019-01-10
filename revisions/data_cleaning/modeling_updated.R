fbbf927292bb168b9f93867adf7e570d6bcf67db
echuu <chuu3ric@gmail.com>
2019-01-09 22:58:02 -0800

    cleanup, done for night

--- revisions/data_cleaning/modeling_updated.R
+++ revisions/data_cleaning/modeling_updated.R
@@ -46,6 +46,7 @@ course_must16 = lm(course ~ ., x_train[,!(names(x0) %in% vars_omit)])
 summary(course_must16)
 must16_coeffs = summary(course_must16)$coefficients
 getMSE(course_must16, x_test, x_test$course) # 112.4562
+# write.csv(must16_coeffs, "model_coeffs/1a_coeffs.csv")
 
 # (1b) course_new: course average ~ new must + . (718 x 13 training matrix)
 vars_omit = c(MUST_q, cats, "pass", "old_must")
@@ -54,6 +55,7 @@ course_must20 = lm(course ~ ., x_train[,!(names(x0) %in% vars_omit)])
 summary(course_must20)
 must20_coeffs = summary(course_must20)$coefficients
 getMSE(course_must20, x_test, x_test$course) # 110.3085
+# write.csv(must20_coeffs, "model_coeffs/1b_coeffs.csv")
 
 
 # (2) model using indicator for each question old vs. new (nested model)
@@ -66,6 +68,7 @@ course_ind16 = lm(course ~ ., x_train[,!(names(x0) %in% vars_omit)])
 summary(course_ind16)
 course_ind16_coeffs = summary(course_ind16)$coefficients
 getMSE(course_ind16, x_test, x_test$course) # 108.3177
+# write.csv(course_ind16_coeffs, "model_coeffs/2a_coeffs.csv")
 
 
 # (2b) course_ind20: course average ~ all 20 must questions (718 x 29 training)
@@ -75,6 +78,8 @@ course_ind20 = lm(course ~ ., x_train[,!(names(x0) %in% vars_omit)])
 summary(course_ind20)
 course_ind20_coeffs = summary(course_ind20)$coefficients
 getMSE(course_ind20, x_test, x_test$course) # 106.9001
+# write.csv(course_ind20_coeffs, "model_coeffs/2b_coeffs.csv")
+
 
 # test for significance between the nested models
 anova(course_ind16, course_ind20) # p = 0.0226 < 0.05 --> extra q's significant
@@ -92,7 +97,7 @@ course_cat = lm(course ~ ., x_train[,!(names(x0) %in% vars_omit)])
 summary(course_cat) # all cats but division significant
 course_cat_coeffs = summary(course_cat)$coefficients
 getMSE(course_cat, x_test, x_test$course) # 108.7652
-
+# write.csv(course_cat_coeffs, "model_coeffs/3_coeffs.csv")
 
 #### -------    lasso reg for modeling course average vs must       ------- ####
 
@@ -128,7 +133,7 @@ coeffs_l0 = predict(course_lasso, type = 'coefficients', s = lambda_star0)
 lasso_pred0  = predict(course_lasso, s = lambda_star0, newx = xtest_mat)
 mean((lasso_pred0 - y_test)^2) # MSE: 107.9899
 
-
+write.csv(as.data.frame(as.matrix(coeffs_l0)), "model_coeffs/4_coeffs.csv")
 
 # (5) lasso: course average ~ category sums 
 # see which categories are indicative of performance
@@ -162,6 +167,8 @@ coeffs_l1 = predict(course_lasso_cats, type = 'coefficients', s = lambda_star1)
 lasso_pred1  = predict(course_lasso_cats, s = lambda_star1, newx = xtest_mat)
 mean((lasso_pred1 - y_test)^2) # MSE: 109.4341
 
+# write.csv(as.data.frame(as.matrix(coeffs_l1)), "model_coeffs/5_coeffs.csv")
+
 
 #### ----    classification for modeling course average old vs. new   ----- ####
 
@@ -181,6 +188,9 @@ pf0_results$test_balance   # test balanced accuracy :  0.6675231
 pf0_results$test_overall   # test overall accuracy  :  0.8028169
 pf0_results$conf_mat       # true values of pass/fail are given by column sums
 
+coeffs_6a = summary(pf0)$coefficients
+# write.csv(as.data.frame(as.matrix(coeffs_6a)), "model_coeffs/6a_coeffs.csv")
+
 
 # (6b) logistic: pass ~ new must + ...
 vars_omit = c(MUST_q, cats, "course", "old_must") # new
@@ -196,6 +206,8 @@ pf1_results$test_balance   # test balanced accuracy :  0.6733159
 pf1_results$test_overall   # test overall accuracy  :  0.7971831
 pf1_results$conf_mat       # true values of pass/fail are given by column sums
 
+coeffs_6b = summary(pf1)$coefficients
+# write.csv(as.data.frame(as.matrix(coeffs_6b)), "model_coeffs/6b_coeffs.csv")
 
 # (7) pass ~ new/old must individual questions (omit category sums) ------------
 
@@ -213,6 +225,9 @@ pf2_results$test_balance   # test balanced accuracy :  0.6941979
 pf2_results$test_overall   # test overall accuracy  :  0.8225352
 pf2_results$conf_mat       # true values of pass/fail are given by column sums
 
+coeffs_7a = summary(pf2)$coefficients
+# write.csv(coeffs_7a, "model_coeffs/7a_coeffs.csv")
+
 
 # (7b) class_indic20: pass ~ 20 old MUST questions + ...
 vars_omit = c(cats, "course", "old_must", "must")
@@ -228,6 +243,8 @@ pf3_results$test_balance   # test balanced accuracy :  0.6711202
 pf3_results$test_overall   # test overall accuracy  :  0.8084507
 pf3_results$conf_mat       # true values of pass/fail are given by column sums
 
+coeffs_7b = summary(pf3)$coefficients
+# write.csv(coeffs_7b, "model_coeffs/7b_coeffs.csv")
 
 # (8) class_cat: pass ~ category sums
 # class_cat
@@ -243,7 +260,8 @@ pf4_results$test_balance   # test balanced accuracy :  0.676913
 pf4_results$test_overall   # test overall accuracy  :  0.8028169
 pf4_results$conf_mat       # true values of pass/fail are given by column sums
 
-
+coeffs_8 = summary(pf4)$coefficients
+# write.csv(coeffs_8, "model_coeffs/8_coeffs.csv")
 
 
 
